f44918432f037d11016aa10638928b100b493e8f
ludger.pottmeier@atos.net <ludger.pottmeier@atos.net>
2019-11-03 23:17:06 +0100

    local

--- import.sh
+++ import.sh
@@ -1,3 +1,4 @@
 # Imports image Stream of Image
 oc import-image registry.redhat.io/openshift3/mediawiki:latest
 oc import-image registry.redhat.io/openshift/ose-base:rhel7
+
---
+++ linksecrets.sh
@@ -0,0 +1,4 @@
+
+oc secrets link builder dockerhub
+oc secrets link builder 11217608-a422171-pull-secret
+oc secrets link default 11217608-a422171-pull-secret
