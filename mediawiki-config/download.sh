75e6e2569a9ac79f995271c0a6d28b3baf380a67
ludger.pottmeier@atos.net <ludger.pottmeier@atos.net>
2019-11-03 14:57:55 +0100

    configure

---
+++ mediawiki-config/download.sh
@@ -0,0 +1,2 @@
+# Download from runninc container
+oc rsync mediawiki-rh:/home/www-data/httpd/mediawiki/includes/DefaultSettings.php .
---
+++ mediawiki-config/import.sh
@@ -0,0 +1,2 @@
+# mount to /home/www-data/httpd/mediawiki/includes/LocalSettings.php
+oc create secret local-settings  --from-file=LocalSettings.php 
